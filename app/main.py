870b7a7e302d7adf1d27c3de1a20c49220225cf4
Alex Keith <alex.michael.keith@gmail.com>
2022-02-24 20:40:58 -0500

    navbar

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
 #import mysql-connector
-from flask import Flask
+from flask import Flask, render_template
 import mysql.connector
 import time
 # Flask constructor takes the name of 
@@ -11,27 +11,22 @@ app = Flask(__name__)
 # The route() function of the Flask class is a decorator, 
 # which tells the application which URL should call 
 # the associated function.
+
+
+
 @app.route('/')
-# ‘/’ URL is bound with hello_world() function.
 def hello_world():
-    #time.sleep(20)
-    connection = mysql.connector.connect(host='localhost',database='OpenCuisine', user = 'root', password = 'root')
-    if connection.is_connected():
-        db_Info = connection.get_server_info()
-        print("Connected to MySQL Server version ", db_Info)
-        cursor = connection.cursor()
-        cursor.execute("select database();")
-        record = cursor.fetchone()
-        print("You're connected to database: ", record)
+    
+    return render_template('home.html')
 
-
-    return "Connected to MySQL Server version"
-
-    #return 'Hello World'
   
+@app.route('/recipes')
+def recipes():
+
+    return render_template('recipes.html')
 # main driver function
 if __name__ == '__main__':
   
     # run() method of Flask class runs the application 
     # on the local development server.
-    app.run(port = 8000, host="0.0.0.0")
+    app.run(port = 5000, host="0.0.0.0", debug=True)
