9f0b6b6fd879e30aebed29966ff4f57095dc8255
Alex Keith <alex.michael.keith@gmail.com>
2022-02-26 17:24:28 -0500

    login creation

--- app/main.py
+++ app/main.py
@@ -2,9 +2,7 @@ from flask import Flask, render_template, flash, redirect, url_for, session, log
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
-import mysql.connector
 import data
-
 
 app = Flask(__name__)
   
@@ -12,11 +10,32 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'myuser'
 app.config['MYSQL_PASSWORD'] = 'mypass'
-app.config['MYSQL_DB'] = 'OpenCuisine'
+app.config['MYSQL_DB'] = 'opencuisine'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 #init MYSQL
 mysql = MySQL(app)
+
+
+def GetArticles():
+
+    cur = mysql.connection.cursor()
+
+    #cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+    cur.execute("use opencuisine")
+    cur.execute("Select * FROM sys.Tables")
+
+    # Commit to DB
+    mysql.connection.commit()
+
+    # Close connection
+    cur.close()
+
+    # Close connection
+    cur.close()
+
 Articles = data.Articles()
+
+#GetArticles()
 
 @app.route('/')
 def hello_world():
@@ -34,6 +53,32 @@ def recipes():
 def article(id):
     return render_template('article.html', id=id)
 
+class AddForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+         name = form.name.data
+
+         #Create Cursor
+         cur = mysql.connection.cursor()
+
+         #cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+         cur.execute("INSERT INTO recipes(name) VALUES(%s)", (name))
+
+         # Commit to DB
+         mysql.connection.commit()
+
+         # Close connection
+         cur.close()
+
+         flash('Recipe has been added', 'success')
+         return redirect(url_for('recipes'))
+
+
+    return render_template('add.html', form=form)
 
 
 class RegisterForm(Form):
@@ -60,7 +105,7 @@ def register():
          cur = mysql.connection.cursor()
 
          #cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
-         cur.execute("INSERT INTO recipes(name) VALUES(%s)", (name))
+         cur.execute("INSERT INTO recipes(name) VALUES(%s)", [name])
 
          # Commit to DB
          mysql.connection.commit()
@@ -73,6 +118,36 @@ def register():
 
 
     return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POSR'])
+def login():
+    if request.method == 'POST':
+        # Get Form Fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Get user by username
+        result = cur.execute("Select * FROM users WHERE username = %s", [username])
+
+        if result > 0:
+            # Get stored hash
+            data = cur.fetchone()
+            password = data['password']
+
+            # Compare Passwords
+            if sha256_crypt.verify(password_candidate, password):
+                app.logger.info('PASSWORD MATCHED')
+            else: 
+                app.logger.info('PASSWORD NOT MATCHED')
+        else:
+            app.logger.info('NO USER')
+    return render_template('login.html')
+
+
 
 # main driver function
 if __name__ == '__main__':
--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,15 @@
-CREATE DATABASE OpenCuisine;
-use OpenCuisine;
+CREATE DATABASE opencuisine;
+use opencuisine;
 
 CREATE TABLE recipes (
-  name VARCHAR(20),
+  name VARCHAR(20)
+);
+
+CREATE TABLE users (
+  id INT(11) AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100),
+  email VARCHAR(100),
+  username VARCHAR(30),
+  password VARCHAR(100),
+  register_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
